948012849e823b2cddc10cd179f0adc1e4f91151
David Beer <davidbeer@Davids-MacBook-Pro.local>
2026-02-05 17:30:43 +0000

    Initial Ktor backend service structure

---
+++ url-shotne-service/settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "url-shotner"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ url-shotne-service/src/main/kotlin/urlshotrner/Application.kt
@@ -0,0 +1,17 @@
+package com.example.urlshortner
+
+import com.example.urlshotrner.config.configureHTTP
+import com.example.urlshotrner.config.configureRouting
+import com.example.urlshotrner.config.configureSerialization
+import io.ktor.server.application.*
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureHTTP()
+    configureRouting()
+}
